773094ad737c9ca76a66eae7e34412479842c950
Guilherme Lourenço <guilhermelourenco.contato@gmail.com>
2024-01-18 10:09:39 +0000

    refactor internal structure and organization for clarity and maintainability

--- Program.cs
+++ Program.cs
@@ -1,22 +1,11 @@
-﻿using Newtonsoft.Json;
-using Microsoft.Extensions.Configuration;
+﻿using CurrencyConverter.Services;
 
 namespace CurrencyConverter
 {
     class Program
     {
-        public static IConfiguration? Configuration { get; set; }
-        public static string? apiKey;
-
         static async Task Main(string[] args)
         {
-            apiKey = GetApiKey();
-
-            if (apiKey == null)
-            {
-                throw new Exception("API key not found");
-            }
-
             if (args.Length != 3)
             {
                 Console.WriteLine("Usage: <amount> <fromCurrency> <toCurrency>");
@@ -28,46 +17,9 @@ namespace CurrencyConverter
             string toCurrency = args[2];
 
             Console.WriteLine($"Converting {amount} {fromCurrency} to {toCurrency}...");
-            decimal convertedAmount = await ConvertCurrency(fromCurrency, toCurrency, amount);
+
+            decimal convertedAmount = await ExchangeRateService.ConvertCurrency(fromCurrency, toCurrency, amount);
             Console.WriteLine($"{amount} {fromCurrency} = {convertedAmount} {toCurrency}");
         }
-
-        static async Task<decimal> ConvertCurrency(string from, string to, decimal amount)
-        {
-            string url = $"https://v6.exchangerate-api.com/v6/{apiKey}/pair/{from}/{to}";
-
-            using (var client = new HttpClient())
-            {
-                var response = await client.GetAsync(url);
-                response.EnsureSuccessStatusCode();
-                var content = await response.Content.ReadAsStringAsync();
-                var result = JsonConvert.DeserializeObject<ExchangeRateResponse>(content);
-
-                decimal rate = result.ConversionRate;
-                return amount * rate;
-            }
-        }
-
-        static string GetApiKey()
-        {
-            var builder = new ConfigurationBuilder()
-                .SetBasePath(Directory.GetCurrentDirectory())
-                .AddJsonFile("appsettings.json", optional: true, reloadOnChange: true);
-
-            Configuration = builder.Build();
-
-            if (Configuration == null)
-            {
-                throw new Exception("Configuration not loaded");
-            }
-
-            return Configuration["ApiKey"];
-        }
-    }
-
-    public class ExchangeRateResponse
-    {
-        [JsonProperty("conversion_rate")]
-        public decimal ConversionRate { get; set; }
     }
 }
